9ce3d83352a0497de9362f9e3f43637bbfe65438
trybulosomaster-max <trybuloso.master@gmail.com>
2026-08-13 20:53:14 -0300

    V12

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE="mentoria-black-v12";
+const APP_SHELL=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  const u=new URL(event.request.url);
+  if(u.origin!==self.location.origin)return;
+  event.respondWith(fetch(event.request).then(r=>{
+    const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html"))));
+});
