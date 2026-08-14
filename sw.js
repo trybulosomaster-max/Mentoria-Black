7f6230013cd8bc56875301deeebcc324e74f03be
trybulosomaster-max <trybuloso.master@gmail.com>
2026-08-14 14:07:58 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE="mentoria-black-v20";
-const APP_SHELL=["./","./index.html","./manifest.webmanifest","./sw.js"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{
- const u=new URL(e.request.url);
- if(u.origin!==self.location.origin||e.request.method!=="GET")return;
- e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{if(r&&r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});}return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
-});
